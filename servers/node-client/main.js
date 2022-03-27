86cf416aa61180f7191d905472984df290cd37f1
atul161 <atuanand199898dbg@gmail.com>
2022-03-27 10:39:09 +0530

    Added client node

---
+++ servers/node-client/main.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const Process = require("process");
+const app = express()
+const axios = require('axios');
+
+async function bootstrap() {
+    console.log("Inside client")
+    const url = process.env.URL
+    while (true){
+        try{
+            await axios.get(url).then(resp => {
+                console.log(resp.data);
+            });
+        }catch (e) {
+           //
+        }
+        console.log("sleeped for one minute")
+        await sleep(1000 * 10)
+    }
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+bootstrap()
